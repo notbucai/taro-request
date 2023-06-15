a5583577ced282991c8c086399ecc9b229aac948
不才 <1450941858@qq.com>
2023-06-15 15:38:32 +0800

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -16,7 +16,7 @@ export interface RequestConfig {
 }
 
 export interface InterceptorManager<V> {
-    use (onFulfilled?: (value: V) => V | Promise<V>, onRejected?: (error: any) => any): number;
+    use (onFulfilled?: (value: V) => any | Promise<any>, onRejected?: (error: any) => any): number;
     eject (id: number): void;
 }
 
